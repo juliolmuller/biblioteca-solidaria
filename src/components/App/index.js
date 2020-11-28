4931f215a8c365636231804e0fbbbcd03ff1164a
Julio Muller <juliodolago@gmail.com>
2020-11-28 14:17:55 -0300

    Projeto criado

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+
+const App = () => (
+  <div id="app">
+    <header id="app-header">
+      <h1 className="text-main">
+        Hello, world!
+      </h1>
+    </header>
+  </div>
+)
+
+export default App
