d21f8bf50327ceefb3f6f74ac47cb54d602d1eb9
Julio Muller <juliodolago@gmail.com>
2020-12-01 01:21:22 -0300

    Renamed 'login' method and added 'logout'

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,33 @@
 import { useState } from 'react'
+import users from '../data/users'
 
 const useAuth = () => {
-  const storage = JSON.parse(localStorage.getItem('user_data'))
-  const [userData, setUserData] = useState(storage)
+  const STORAGE_KEY = 'user_data'
+  const storage = JSON.parse(localStorage.getItem(STORAGE_KEY))
+  const [userData] = useState(storage)
   const isAuthenticated = Boolean(storage)
 
-  const authenticate = (data) => {
-    localStorage.setItem('user_data', JSON.stringify(data))
-    setUserData(data)
+  const refreshPage = () => {
+    // eslint-disable-next-line no-self-assign
+    location.href = location.href
   }
 
-  return { userData, isAuthenticated, authenticate }
+  const login = (username, password) => {
+    const user = users.find((u) => u.username === username && u.password === password)
+
+    if (user) {
+      delete user.password
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(user))
+      refreshPage()
+    }
+  }
+
+  const logout = () => {
+    localStorage.removeItem(STORAGE_KEY)
+    refreshPage()
+  }
+
+  return { userData, isAuthenticated, login, logout }
 }
 
 export default useAuth
