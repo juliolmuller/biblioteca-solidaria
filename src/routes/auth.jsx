184f18d2935814fb46a1e13ba7b84273701d5a20
Julio Muller <juliodolago@gmail.com>
2020-12-05 14:44:21 -0300

    Fixed router imports and components lookup order

--- src/routes/auth.jsx
+++ src/routes/auth.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import Home from '../pages/Home'
-import BookForm from '../pages/BookForm'
 import BookDetails from '../pages/BookDetails'
+import BookForm from '../pages/BookForm'
+import UserDetails from '../pages/UserDetails'
 import UserForm from '../pages/UserForm'
-import UserDetails from '../pages/BookDetails'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/livro/:id" component={BookForm} />
-      <Route path="/livro/novo" component={BookDetails} />
+      <Route path="/livro/novo" component={BookForm} />
       <Route path="/livro/editar/:id" component={BookForm} />
-      <Route path="/usuario" component={UserDetails} />
+      <Route path="/livro/:id" component={BookDetails} />
+      <Route path="/usuario" exact component={UserDetails} />
       <Route path="/usuario/editar" component={UserForm} />
 
       <Redirect to="/" />
