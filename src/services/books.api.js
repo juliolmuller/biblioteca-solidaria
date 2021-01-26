7dd88728447585881f3833387ac2534100a4177a
Julio Muller <juliodolago@gmail.com>
2021-01-26 01:06:23 -0300

    Added "endpoint" to update book record

--- src/services/books.api.js
+++ src/services/books.api.js
@@ -3,6 +3,21 @@ import originalBooks from '../data/books'
 import { find as findUser } from './users.api'
 import { deferCall } from '../utils/fake-api'
 
+const extractData = (bookData) => ({
+  description: bookData.description,
+  testimony: bookData.testimony,
+  situation: bookData.situation,
+  publisher: bookData.publisher,
+  language: bookData.language,
+  edition: bookData.edition,
+  images: bookData.images,
+  author: bookData.author,
+  pages: bookData.pages,
+  title: bookData.title,
+  owner: bookData.owner,
+  year: bookData.year,
+})
+
 export const make = (props) => ({
   ...props,
   id: originalBooks.reduce((max, { id }) => Math.max(max, id), 0) + 1,
@@ -22,35 +37,23 @@ export const find = async (bookId) => {
 
 export const create = (bookData) => {
   return deferCall(() => {
-    const {
-      year,
-      title,
-      pages,
-      images,
-      author,
-      edition,
-      language,
-      publisher,
-      situation,
-      testimony,
-      description,
-    } = bookData
-    const newBook = make({
-      description,
-      testimony,
-      situation,
-      publisher,
-      language,
-      edition,
-      images,
-      author,
-      pages,
-      title,
-      year,
-    })
+    const book = extractData(bookData)
+    const newBook = make(book)
 
     originalBooks.push(newBook)
 
     return newBook
-  }, { min: 100, max: 1000 })
+  })
+}
+
+export const update = (bookId, bookData) => {
+  return deferCall(() => {
+    const existingBook = originalBooks.find(({ id }) => bookId === id)
+    const bookIndex = originalBooks.indexOf(existingBook)
+    const newBook = { id: bookId, ...extractData(bookData) }
+
+    originalBooks[bookIndex] = newBook
+
+    return newBook
+  })
 }
