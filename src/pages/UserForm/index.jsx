0682b785959c84db7d6025a08fcfc39163733bf4
Julio Muller <juliodolago@gmail.com>
2021-01-29 23:41:29 -0300

    Finished visuals for user form view

--- src/pages/UserForm/index.jsx
+++ src/pages/UserForm/index.jsx
@@ -1,19 +1,123 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import Header from '../../components/TheHeader'
 import HorizontalLine from '../../components/HorizontalLine'
+import Input from '../../components/Input'
+import { useAuth, useUsersApi } from '../../hooks'
+import * as masks from '../../utils/input-masks'
 import './styles.scss'
 
-const UserForm = () => (
-  <div id="user-form">
-    <Header searchControls />
+const UserForm = () => {
+  const router = useHistory()
+  const { userData } = useAuth()
+  const { isLoading } = useUsersApi()
+  const [email, setEmail] = useState(userData.emails[0])
+  const [avatar, setAvatar] = useState(userData.avatar)
+  const [lastName, setLastName] = useState(userData.lastName)
+  const [firstName, setFirstName] = useState(userData.firstName)
+  const [dateOfBirth, setDateOfBirth] = useState(userData.dateOfBirth)
+  const [phoneNumber, setPhoneNumber] = useState(userData.phoneNumber)
+  const [isTelegram, setIsTelegram] = useState(userData.isTelegram)
+  const [isWhatsApp, setIsWhatsApp] = useState(userData.isWhatsApp)
+  const [registration] = useState(userData.registration)
 
-    <div className="container">
-      <HorizontalLine goBackButton />
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
 
-      <h1 className="page-title">
-        Hello, UserForm!
-      </h1>
+  return (
+    <div id="user-form">
+      <Header searchControls />
+
+      <div className="container">
+        <HorizontalLine goBackButton />
+
+        <h1 className="page-title">
+          Atualizando Dados de {userData.firstName}
+        </h1>
+
+        <form onSubmit={handleSubmit}>
+          <Input
+              label="Primeiro nome:"
+              model={[firstName, setFirstName]}
+              mask={masks.name}
+              required
+              autoFocus
+            />
+            <Input
+              label="Sobrenome:"
+              model={[lastName, setLastName]}
+              mask={masks.name}
+              required
+            />
+            <Input
+              type="avatar"
+              label="Foto do usuário:"
+              model={[avatar, setAvatar]}
+              required
+            />
+            <Input
+              label="Data de nascimento:"
+              placeholder="dd/mm/aaaa"
+              model={[dateOfBirth, setDateOfBirth]}
+              mask={masks.date}
+              required
+            />
+            <Input
+              type="tel"
+              label="Telefone de contato:"
+              placeholder="(00) 0000-0000"
+              model={[phoneNumber, setPhoneNumber]}
+              mask={masks.phone}
+            />
+            <Input
+              label=" "
+              type="checkbox"
+              checkboxLabel="Este número de telefone também é Telegram"
+              model={[isTelegram, setIsTelegram]}
+            />
+            <Input
+              label=" "
+              type="checkbox"
+              checkboxLabel="Este número de telefone também é WhatsApp"
+              model={[isWhatsApp, setIsWhatsApp]}
+            />
+            <Input
+              type="email"
+              label="Email da UFPR:"
+              placeholder="exemplo@email.com"
+              model={[email, setEmail]}
+              required
+            />
+            <Input
+              label="Matrícula (GRR ou outro):"
+              placeholder="GRR00000000"
+              model={[registration]}
+              mask={masks.grr}
+              readOnly
+            />
+
+            <div className="action-buttons">
+              <button
+                type="submit"
+                className="btn btn-lg btn-secondary"
+                disabled={isLoading}
+              >
+                <i className="fas fa-save mr-2"></i>
+                Salvar Alterações
+              </button>
+
+              <button
+                type="button"
+                className="btn btn-lg btn-link"
+                onClick={() => router.goBack()}
+                disabled={isLoading}
+              >Cancelar e retornar à página anterior</button>
+            </div>
+        </form>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default UserForm
